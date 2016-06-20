443786977f1acfa93916560997c13339ae82a9b6
Alex Pimania <randomiseme33@gmail.com>
2016-06-20 16:03:03 +1000

    More images for UI and get average price function
    This update adds about 6 new image handlers so that the UI can use them as icons and also adds a new feature to the API which allows you to get the average last price for a specified list of exchanges.
    
    -Alex :)

--- backend/beeWebServices.py
+++ backend/beeWebServices.py
@@ -1,48 +1,48 @@
 import sys
 import os
 BEE_PATH = sys.argv[1] + "/frontend"
-Orig_BEE_PATH = sys.argv[1] 
+Orig_BEE_PATH = sys.argv[1]
 port = sys.argv[2]
 sys.path.append("/usr/lib/python3/dist-packages")
 
 import beeExchangeServices as beeServices
 import tornado.ioloop
 import tornado.web
-import tornado.httpserver        
+import tornado.httpserver
 class HTMLHandler(tornado.web.RequestHandler):
     def get(self):
         self.render(BEE_PATH + "/beehtml.html")
-  
+
 class JSHandler(tornado.web.RequestHandler):
     def get(self):
         self.render(BEE_PATH + "/beejs.js")
-        
+
 class CSSHandler(tornado.web.RequestHandler):
     def get(self):
         self.set_header('Content-Type', 'text/css')
         self.render(BEE_PATH + "/beecss.css")
-        
-                     
+
+
 class homeHandler(tornado.web.RequestHandler):
     # This routing table directs off to the services to support the UI.
-    # Route to the appropriate function, passing in parameters as required.    
+    # Route to the appropriate function, passing in parameters as required.
     def get(self):
         beePassword = open(Orig_BEE_PATH + "/backend/password.txt").read().strip()
         if self.get_argument("password", "") == beePassword:
             if self.get_argument("function", "") == "getProfileTextFile":
-                self.write(beeServices.getProfileTextFile(self.get_argument("profileName", ""), self.get_argument("newLineChar", "<br>"))) 
+                self.write(beeServices.getProfileTextFile(self.get_argument("profileName", ""), self.get_argument("newLineChar", "<br>")))
         else:
             self.write("Invalid Password")
-                 
+
     def post(self):
         beePassword = open(Orig_BEE_PATH + "/backend/password.txt").read().strip()
         arguments = {}
         for NVP in self.request.body.decode('utf-8').split("&"):
             name, value = NVP.split("=")
             arguments[name] = value
-            
+
         if arguments["password"] == beePassword:
-            if arguments["function"] == "getProfileNameList":               
+            if arguments["function"] == "getProfileNameList":
                 self.write(beeServices.getProfileNameList())                                    # INPUT: No params
             elif arguments["function"] == "getProfileDetails":
                 self.write(beeServices.getProfileDetails(arguments["profileName"]))     # INPUT: string of profileName
@@ -58,19 +58,21 @@ class homeHandler(tornado.web.RequestHandler):
 
             elif arguments["function"] == "getProfileStats":
                 self.write(beeServices.getProfileStats(arguments["profileName"]))       # INPUT: string of profileName
-                
+
             elif arguments["function"] == "getProfileStatsText":
                 self.write(beeServices.getProfileStatsText(arguments["profileName"]))    # INPUT: string of profileName
-                
+
             elif arguments["function"] == "statstEngine":
                 self.write(beeServices.statsEngine(arguments["action"]))                   #starts and stops stats engine
-                
+
             elif arguments["function"] == "getExchangeDetails":
                 self.write(beeServices.getExchangeDetails(arguments["exchangeName"]))
+            elif arguments["function"] == "getAveragePrice":
+                self.write(beeServices.getCurrentPrice(arguments["exchanges"]))
         else:
             self.write("Invalid Password")
-        
-        
+
+
 application = tornado.web.Application([
     (r"/beeServices/", homeHandler),
     (r"/", HTMLHandler),
@@ -80,6 +82,12 @@ application = tornado.web.Application([
     (r"/ShareTech.ttf()", tornado.web.StaticFileHandler, {'path': BEE_PATH + '/ShareTech.ttf'}),
     (r"/bee.gif()", tornado.web.StaticFileHandler, {'path': BEE_PATH + '/bee.gif'}),
     (r"/beeBackground.jpg()", tornado.web.StaticFileHandler, {'path': BEE_PATH + '/beeBackground.jpg'}),
+    (r"/documentationIcon.png()", tornado.web.StaticFileHandler, {'path': BEE_PATH + '/documentationIcon.png'}),
+    (r"/improvementsIcon.png()", tornado.web.StaticFileHandler, {'path': BEE_PATH + '/improvementsIcon.png'}),
+    (r"/maximiseIcon.png()", tornado.web.StaticFileHandler, {'path': BEE_PATH + '/maximiseIcon.png'}),
+    (r"/minimiseIcon.png()", tornado.web.StaticFileHandler, {'path': BEE_PATH + '/minimiseIcon.png'}),
+    (r"/priceIcon.png()", tornado.web.StaticFileHandler, {'path': BEE_PATH + '/priceIcon.png'}),
+    (r"/refreshIcon.png()", tornado.web.StaticFileHandler, {'path': BEE_PATH + '/refreshIcon.png'}),
 ], ssl_options={
     "certfile": os.path.join(Orig_BEE_PATH + "/ssl/cert.pem"),
     "keyfile": os.path.join(Orig_BEE_PATH + "/ssl/privkey.pem"),
@@ -90,7 +98,7 @@ http_server = tornado.httpserver.HTTPServer(application, ssl_options={
     "keyfile": os.path.join(Orig_BEE_PATH + "/ssl/privkey.pem"),
 })
 
-    
+
 if True:
 	http_server.listen(port)
 	tornado.ioloop.IOLoop.instance().start()
